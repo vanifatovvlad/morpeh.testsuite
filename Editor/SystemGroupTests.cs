14ac62a7e892d22e3055470a075e9b65b37874d7
VladV <vanifatov.vlad@yandex.ru>
2022-08-06 10:31:50 +0300

    System group dispose tests

---
+++ Editor/SystemGroupTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Morpeh.TestSuite.Editor {
+    using NUnit.Framework;
+
+    [TestFixture(Category = "Morpeh/SystemGroups")]
+    public class SystemGroupTests {
+        private const float DELTA_TIME = 0.1f;
+
+        private World world;
+
+        [SetUp]
+        public void Setup() => this.world = World.Create();
+
+        [TearDown]
+        public void TearDown() {
+            this.world.Dispose();
+            this.world = null;
+        }
+
+        [Test]
+        public void Systems_Disposed_On_SystemGroup_Removal() {
+            var disposeCalls = 0;
+
+            var system = new TestUpdateSystem {
+                DisposeAction = f => ++disposeCalls,
+            };
+
+            var systemGroup = this.world.CreateSystemsGroup();
+            systemGroup.AddSystem(system);
+            this.world.AddSystemsGroup(0, systemGroup);
+
+            this.world.Update(DELTA_TIME);
+
+            this.world.RemoveSystemsGroup(systemGroup);
+
+            Assert.AreEqual(1, disposeCalls);
+        }
+    }
+}
